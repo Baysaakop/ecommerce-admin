9c29ccb7ff0b44ab40d678b2ebbbebbb1d85f45f
Baysaakop <baysaakop@gmail.com>
2023-09-22 16:52:36 +0800

    Form components (Admin)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,7 @@ import { useStoreModal } from '@/hooks/use-store-modal';
 
 const SetupPage = () => {
     const onOpen = useStoreModal((state) => state.onOpen);
-    const isOpen = useStoreModal((state) => state.isOpen);
+    const isOpen = useStoreModal((state) => state.isOpen); 
 
     useEffect(() => {
         if (!isOpen) {
